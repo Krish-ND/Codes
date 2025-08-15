0ffe67604e4b6874e4a00cbef1868554ca016e86
Krish-ND <selfstrivingme@gmail.com>
2025-08-15 23:24:05 +0530

    Checking for errors

--- Python/HelloWorld.py
+++ Python/HelloWorld.py
@@ -1,3 +1,4 @@
 print("Hello World")
 print("This is my first line of code")
 print("My name is Krishna")
+#Comment Line
--- java/Solution.java
+++ java/Solution.java
@@ -13,6 +13,7 @@ public class Solution {
             }
             if (count > max)
                 max = count;
+            // Updating the count value as max
         }
         return max;
     }
