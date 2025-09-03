50d0a6e0199f055ea80c0e685210c94dacce8e34
Krish-ND <selfstrivingme@gmail.com>
2025-09-04 00:07:33 +0530

    GCD of Two Numbers

---
+++ java/GCDChecker.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class GCDChecker {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int res = arr[0];
+        for (int i = 1; i < n; i++) {
+            res = gcd(res, arr[i]);
+        }
+        System.out.println(res);
+    }
+}
